f57e83a6709bea206fff732d2401ec42f9565085
NaderAlAwar <naderalawar@gmail.com>
2019-03-08 11:21:24 +0200

    service now receives json from request

--- app/backtesting/routes.py
+++ app/backtesting/routes.py
@@ -2,8 +2,10 @@ from app.backtesting import bp
 from flask import request
 from get_data import get_data
 from flask import jsonify
+from get_statistics import get_statistics
 
 @bp.route("/api/backtest", methods=['POST'])
 def get_stock_ticker_data():
-    portfolio = request.get_json()
+    portfolio = request.get_json(force=True)
+    get_statistics(portfolio)
     return jsonify(portfolio)
